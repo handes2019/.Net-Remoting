13e6b88fe1bf57b35b0135bb70182f890185c4d1
handes <2451322819@qq.com>
2020-02-08 17:53:34 +0800

    1.0.0

--- ConsoleApplication1/TestRemotingClient/Program.cs
+++ ConsoleApplication1/TestRemotingClient/Program.cs
@@ -1,4 +1,10 @@
-﻿namespace TestRemotingClient
+﻿using System;
+using System.Runtime.Remoting;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting.Channels.Http;
+using System.Threading;
+
+namespace TestRemotingClient
 {
     /// <summary>
     /// 客户端
@@ -7,6 +13,22 @@
     {
         public static void Main(string[] args)
         {
+            /*创建通道*/
+            HttpChannel channel = new HttpChannel();
+            /*注册通道*/
+            ChannelServices.RegisterChannel(channel, false); 
+            /*注册通道 的 远程处理类型*/
+            RemotingConfiguration.RegisterWellKnownClientType(typeof(MessageMarshal.TestMessageMarshal), "http://localhost:816/TestMessageMarshal");  
+            /*创建消息实体*/
+            MessageMarshal.TestMessageMarshal TestMessage = new MessageMarshal.TestMessageMarshal();
+
+            while (true)
+            {
+                
+                TestMessage.SendMessage("DateTime.Now:" + System.DateTime.Now.ToString());
+                Console.WriteLine("send message...");
+                Thread.Sleep(2000);
+            }
         }
     }
 }
